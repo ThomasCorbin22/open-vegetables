6f9431bedd7167a9ceffd89838aa227d42f69aff
YIN <anotherone@YINde-MacBook-Air.local>
2020-06-08 12:26:45 +0800

    main layouts

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const https = require('https');
-
+const hbs = require('express-handlebars')
 // Specify public directory
 const publicDirectory = __dirname + '/public'
 
 // Require router modules
-const userRouter = require('./router/UserRouter')(express, publicDirectory);
-const restaurantRouter = require('./router/RestaurantRouter')(express, publicDirectory);
-const blogRouter = require('./router/BlogRouter')(express, publicDirectory);
+// const userRouter = require('./router/UserRouter')(express, publicDirectory);
+// const restaurantRouter = require('./router/RestaurantRouter')(express, publicDirectory);
+// const blogRouter = require('./router/BlogRouter')(express, publicDirectory);
 
 // Require passport initialisation
 const initPassport = require('./passport/init-passport');
@@ -27,6 +27,9 @@ const server = https.createServer({
 },app);
 
 // Set up middleware
+app.engine('handlebars',hbs({defaultLayout:'main',layoutsDir:__dirname+'/public/views/layouts'}))
+app.set('view engine','handlebars')
+app.set('views',__dirname+'/public/views')
 app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -36,13 +39,17 @@ app.use(session({
     saveUninitialized: true,
 }));
 
+app.get('/',(req,res)=>{
+    res.render('index',{title:'Home'})
+})
+
 // Initialise passport
-initPassport(app);
+// initPassport(app);
 
 // Set up routers
-app.use('/user', userRouter);
-app.use('/restaurant', restaurantRouter);
-app.use('/blog', blogRouter);
+// app.use('/user', userRouter);
+// app.use('/restaurant', restaurantRouter);
+// app.use('/blog', blogRouter);
 
 // Set up server
 server.listen(port);

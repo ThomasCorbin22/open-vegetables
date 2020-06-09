0f1d8ed6669286317000b5623c48c400d63def80
YIN <anotherone@YINde-MacBook-Air.local>
2020-06-09 15:55:37 +0800

    Fixed minor bugs on public folder

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const server = https.createServer({
 // Set up middleware
 app.engine('handlebars',hbs({defaultLayout:'main'}))
 app.set('view engine','handlebars')
+
 app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -49,36 +50,31 @@ app.use(session({
 }));
 
 app.get('/',(req,res)=>{
-    res.render('test',{title:'Home'})
+    res.render('index',{title:'Home'})
 })
 
 app.get('/restaurants/All',(req,res)=>{
     res.render('restaurant',{title:'Restaurants-All'})
 })
 
-app.get('/user/info',(req,res)=>{
+app.get('/users/info',(req,res)=>{
     res.render('user_information',{title:'userInformation'})
 })
 
-app.get('/user/reviews',(req,res)=>{
+app.get('/users/reviews',(req,res)=>{
     res.render('user_reviews',{title:'userReviews'})
 })
 
-app.get('/user/blogs',(req,res)=>{
+app.get('/users/blogs',(req,res)=>{
     res.render('user_blogs',{title:'userBlogs'})
 })
 
-app.get('/user/restaurants',(req,res)=>{
+app.get('/users/restaurants',(req,res)=>{
     res.render('user_restaurants',{title:'userRestaurants'})
 })
 
 // Initialise passport
 initPassport(app);
-
-// Send index page
-app.get('/', (req, res) => {
-    res.sendFile(publicDirectory + '/index.html');
-});
 
 // Set up routers
 app.use('/user', new UserRouter(new UserService()).route());

88e74ddcb419540ad58c3a2c53a06909351471cf
Thomas Corbin <thomas.corbin22@gmail.com>
2020-06-19 18:31:14 +0800

    Fix map search

--- public/js/map.js
+++ public/js/map.js
@@ -3,10 +3,15 @@ let districts = {
     wanchai: {lat: 22.276, lng:114.1751},
 }
 
-function extractsDistrictFromURL(){
+function extractsDistrict(){
     let url = window.location.href
     let spliturl = url.split('/')[6]
-    console.log(spliturl)
+    return spliturl
+}
+
+function extractsLatLng(){
+    let url = window.location.href
+    let spliturl = url.split('/')[6]
     return districts[spliturl]
 }
 
@@ -14,8 +19,30 @@ function initMap() {
     // Map options
     var options = {
         zoom: 15,
-        center: extractsDistrictFromURL()
+        center: extractsLatLng() || {lat: 22.3193, lng: 114.1694}
     }
     // New map
     var map = new google.maps.Map(document.getElementById('map'), options);
 }
+
+console.log('/restaurant/search?district=' + extractsDistrict())
+
+// Search all restaurants
+axios({
+    url: '/restaurant/search?district=' + extractsDistrict(),
+    method: 'get'
+})
+.then((res) => {
+    // res.data will have all of the corresponding restaurants that have correct area and district
+    console.log(res.data)
+
+    for (let restaurant in res.data){
+        let lat = restaurant.latitude
+        let lng = restaurant.longitude
+
+        // Add a marker to the google map
+    }
+})
+.catch((error) => {
+    console.log(error);
+})
